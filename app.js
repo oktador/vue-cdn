60d91b1de3289c1a16c7a53b9df0a8517293b4c4
oktador <oktay.doruk@verion.com.tr>
2023-10-03 01:23:40 +0300

    note6 commited

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
 const app = Vue.createApp({
   data() {
     return {
-      info1: "info1",
-      info2: "info2",
-      number: 0,
-      showInfos: true,
-      buttonTitle: "Hide Infos",
+      url: "http://www.oktador.com",
     };
-  },
-  methods: {
-    showHide() {
-      this.showInfos = !this.showInfos;
-      this.buttonTitle = this.showInfos ? "Hide Infos" : "Show Infos";
-    },
   },
 });
 
