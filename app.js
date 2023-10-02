74dfc830ccdad7fd00065308786edbb028f3b28f
oktador <oktay.doruk@verion.com.tr>
2023-10-03 01:27:34 +0300

    note7 commited

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 const app = Vue.createApp({
   data() {
     return {
-      url: "http://www.oktador.com",
+      books: [
+        {
+          title: "title1",
+          author: "author1",
+          img: "./assets/img/1.png",
+          isFav: true,
+        },
+        {
+          title: "title2",
+          author: "author2",
+          img: "./assets/img/2.png",
+          isFav: false,
+        },
+        {
+          title: "title3",
+          author: "author3",
+          img: "./assets/img/3.png",
+          isFav: false,
+        },
+        {
+          title: "title4",
+          author: "author4",
+          img: "./assets/img/4.png",
+          isFav: true,
+        },
+        {
+          title: "title5",
+          author: "author5",
+          img: "./assets/img/5.png",
+          isFav: false,
+        },
+      ],
     };
   },
 });
